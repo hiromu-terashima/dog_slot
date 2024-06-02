85a0d28724dc1c3ddb86aee20229851615277779
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2024-06-02 23:58:21 +0900

    コメント 変数名　

--- ramdam.js
+++ ramdam.js
@@ -1,9 +1,9 @@
-const SLOT_ARRAY = ['ポメ', 'ラニ', 'アン'] //スロットの中身
+const SLOT_ARRAY = ['ポメ', 'ラニ', 'アン'] //スロットの中身を格納する定数配列
 let slot_cnt = 0 //スロットの回数
-let slot_result_array = [] //獲得したスロットの値
-var clear_chk_array = []
+let slot_result_array = [] //獲得したスロットの値を格納する配列
+let clear_chk_array = []　//全てのパターンを網羅したか確認するための配列
 
-//スロットを止める
+//1～3のランダム値を獲得し、引数の位置に結果を格納する
 function stop_slot(slot_num) {
     ram_int = Math.floor(Math.random() * (2 + 1))
     slot_result_array[slot_num] = SLOT_ARRAY[ram_int]
@@ -15,15 +15,16 @@ function stop_slot(slot_num) {
 function slot_limit_chk() {
     //スロットをすべて回していた場合実行
     if (slot_cnt === 3) {
-        //江戸っ子が完成している場合
+        //ポメラニアンが完成している場合
         if (JSON.stringify(SLOT_ARRAY) === JSON.stringify(slot_result_array)) {
             result = ('ポメラニアン!!!!!')
             result_img_path = 'img/ポメラニアン成功.jpg'
             score_num = 1
 
-            //同じ単語が揃っている場合
+            //同じ単語が揃っている場合(例:ポメ,ポメ,ポメ)
         } else if (slot_result_array[0] === slot_result_array[1] && slot_result_array[0] === slot_result_array[2]) {
             result = ('ポメラニアン？')
+            //ランダムで失敗画像,パターン番号を格納
             false_num = Math.floor(Math.random() * (2 + 1))
             if (false_num === 0) {
                 result_img_path = 'img/おしい_1.jpg'
@@ -36,12 +37,11 @@ function slot_limit_chk() {
                 score_num = 4
             }
 
-            //はずれの場合
+            //上記のどれにも当てはまらない場合
         } else {
-            false_num = Math.floor(Math.random() * (2 + 1))
             result = ('ポメラニアン失敗')
-            console.log(false_num)
-            //ランダムで失敗画像を表示
+            //ランダムで失敗画像,パターン番号を格納
+            false_num = Math.floor(Math.random() * (2 + 1))
             if (false_num === 0) {
                 result_img_path = 'img/失敗_1.jpg'
                 score_num = 5
@@ -52,9 +52,8 @@ function slot_limit_chk() {
                 result_img_path = 'img/失敗_3.jpg'
                 score_num = 7
             }
-
         }
-        //結果を表示
+        //結果を反映
         view_result()
         update_score()
     }
@@ -63,36 +62,34 @@ function slot_limit_chk() {
 
 //スロットの値をそれぞれ表示
 function slot_view(div_nm, i) {
-    var resultDiv = document.getElementById(div_nm);
-    resultDiv.innerHTML = slot_result_array[i]
+    document.getElementById(div_nm).innerHTML = slot_result_array[i]
 }
 
 //スロットの結果を表示
 function view_result() {
-    var resultDiv = document.getElementById("result_str")
-    resultDiv.innerHTML = result
-    var resultDiv = document.getElementById("result_img")
-    resultDiv.innerHTML = '<img src="' + result_img_path + '" ></img>;'
+    document.getElementById("result_str").innerHTML = result
+    document.getElementById("result_img").innerHTML = '<img src="' + result_img_path + '" ></img>;'
 }
 
 //戦績を更新する
 function update_score() {
-    var scoreDiv = document.getElementById("score_" + score_num)
-    scoreDiv.setAttribute('src', result_img_path)
+    document.getElementById("score_" + score_num).setAttribute('src', result_img_path)
+    //配列に結果を格納する
     clear_chk_array[score_num] = score_num
+    //7パターンすべての結果が格納されている場合、コンプリートと表示する。
     if (clear_chk_array.filter(item => item !== undefined).length === 7) {
-        document.getElementById('title').textContent = "クリア！！！！！";
+        document.getElementById('title').textContent = "コンプリート！！！";
     }
 
 }
 
-//リセット機能
+//リセット機能(戦績はリセットされない)
 function reset() {
     slot_cnt = 0
     slot_result_array = []
-    document.getElementById('STOP1').disabled = false;
-    document.getElementById('STOP2').disabled = false;
-    document.getElementById('STOP3').disabled = false;
+    document.getElementById('PUSH1').disabled = false;
+    document.getElementById('PUSH2').disabled = false;
+    document.getElementById('PUSH3').disabled = false;
     document.getElementById('1st').textContent = "";
     document.getElementById('2nd').textContent = "";
     document.getElementById('3rd').textContent = "";
@@ -101,45 +98,39 @@ function reset() {
 
 }
 
-//STOP1ボタン
-const SLOT_STOP_EVENT_1 = document.getElementById('STOP1');
-SLOT_STOP_EVENT_1.addEventListener('click', () => {
+//PUSH1ボタン
+document.getElementById('PUSH1').addEventListener('click', () => {
     stop_slot(0)
     slot_view("1st", 0)
     slot_limit_chk()
 });
 
-//STOP2ボタン
-const SLOT_STOP_EVENT_2 = document.getElementById('STOP2');
-SLOT_STOP_EVENT_2.addEventListener('click', () => {
+//PUSH2ボタン
+document.getElementById('PUSH2').addEventListener('click', () => {
     stop_slot(1)
     slot_view("2nd", 1)
     slot_limit_chk()
 });
 
-//STOP3ボタン
-const SLOT_STOP_EVENT_3 = document.getElementById('STOP3');
-SLOT_STOP_EVENT_3.addEventListener('click', () => {
+//PUSH3ボタン
+document.getElementById('PUSH3').addEventListener('click', () => {
     stop_slot(2)
     slot_view("3rd", 2)
     slot_limit_chk()
 });
 
 //リセットボタン
-const reset_event = document.getElementById('reset');
-reset_event.addEventListener('click', () => {
+document.getElementById('reset').addEventListener('click', () => {
     reset()
 });
 
-//遊び方
-const help_event = document.getElementById('help');
-help_event.addEventListener('click', () => {
+//遊び方ボタン
+document.getElementById('help').addEventListener('click', () => {
     alert('【ポメ】【ラニ】【アン】を揃えてポメラニアンを出現させよう！！！');
 });
 
-//戦績
-const score_event = document.getElementById('score');
-score_event.addEventListener('click', () => {
+//戦績画面遷移ボタン
+document.getElementById('score').addEventListener('click', () => {
     document.getElementById('title').style.display = 'block';
     document.getElementById('score_view').style.display = 'block';
     document.getElementById('slot_view').style.display = 'none';
@@ -150,9 +141,8 @@ score_event.addEventListener('click', () => {
 
 });
 
-//戦績
-const score2_event = document.getElementById('slot');
-score2_event.addEventListener('click', () => {
+//スロット画面遷移ボタン
+document.getElementById('slot').addEventListener('click', () => {
     document.getElementById('title').style.display = 'none';
     document.getElementById('score_view').style.display = 'none';
     document.getElementById('slot_view').style.display = 'block';
